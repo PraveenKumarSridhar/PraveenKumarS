2cee6abd6ab6d58aa846114280d01790eb4ac81b
Praveen Kumar Sridhar <prasri.pk@gmail.com>
2022-01-16 20:17:45 -0500

    improvements to the particle playground

--- js/jquery.particleground.js
+++ js/jquery.particleground.js
@@ -374,9 +374,9 @@
 
   window[pluginName].defaults = {
     minSpeedX: 0.1,
-    maxSpeedX: 0.7,
+    maxSpeedX: 0.75 ,
     minSpeedY: 0.1,
-    maxSpeedY: 0.7,
+    maxSpeedY: 0.75,
     directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
     directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
     density: 10000, // How many particles will be generated: one particle every n pixels
@@ -385,9 +385,9 @@
     particleRadius: 7, // Dot size
     lineWidth: 1,
     curvedLines: false,
-    proximity: 100, // How close two dots need to be before they join
+    proximity: 90, // How close two dots need to be before they join
     parallax: true,
-    parallaxMultiplier: 5, // The lower the number, the more extreme the parallax effect
+    parallaxMultiplier: 4, // The lower the number, the more extreme the parallax effect
     onInit: function() {},
     onDestroy: function() {}
   };
